40d369bceef5deff923ea5dd1b276c9cd9a108a9
conradbuys11 <conradbuys11@gmail.com>
2022-03-05 14:55:22 -0500

    adding bcrypt support, need to figure it out further

--- src/main/java/com/friendkeeper/config/SecurityConfig.java
+++ src/main/java/com/friendkeeper/config/SecurityConfig.java
@@ -3,11 +3,14 @@ package com.friendkeeper.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -15,16 +18,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private DataSource secDataSource;
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.jdbcAuthentication().dataSource(secDataSource);
+		auth.jdbcAuthentication().dataSource(secDataSource).passwordEncoder(passwordEncoder());
 	}
 
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		// TODO authorizing requests for certain roles?
-//	}
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO authorizing requests for certain roles?
+		http.authorizeRequests()
+		.anyRequest().permitAll();
+		
+		http.csrf().disable();
+	}
 	
 	
 	
